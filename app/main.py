afe506f77a0bd093ed81283b204461f83d16e9cf
Louis Sobel <sobel@mit.edu>
2014-04-24 17:00:00 -0400

    data page skeleton

--- app/main.py
+++ app/main.py
@@ -14,5 +14,9 @@ def index():
 def add_item():
     return flask.render_template('add_item.html')
 
+@app.route('/data')
+def data():
+    return flask.render_template('data.html')
+
 if __name__ == "__main__":
     app.run('0.0.0.0', port=6813)
