2f8b9d53e2d12e90a945426e701c69a493579789
Tsotne Tabidze <tsotne@Tsotnes-MacBook-Air.local>
2014-04-28 13:58:02 -0400

    Added graphs for the data visualization

--- app/static/js/views/highlights_graph.js
+++ app/static/js/views/highlights_graph.js
@@ -11,7 +11,32 @@ var HighlightsGraph = Backbone.View.extend({
   }
 
 , render: function () {
-    this.$el.html(this.dataDateRange.get("mode") + " " + this.type + " highlights");
+//    this.$el.html(this.dataDateRange.get("mode") + " " + this.type + " highlights");
+    $canvas = this.$el.find(".canvas");
+
+
+	var barChartData = {
+		labels : ["January","February","March","April","May","June","July"],
+		datasets : [
+			{
+				fillColor : "rgba(220,220,220,0.5)",
+				strokeColor : "rgba(220,220,220,1)",
+				data : [65,59,90,81,56,55,40]
+			},
+			{
+				fillColor : "rgba(151,187,205,0.5)",
+				strokeColor : "rgba(151,187,205,1)",
+				data : [28,48,40,19,96,27,100]
+			}
+		]
+		
+	}
+
+	$canvas.attr('height', this.$el.css('height'));
+	$canvas.attr('width', this.$el.css('width'));
+	this.chart = new Chart($canvas[0].getContext("2d")).Bar(barChartData);
+
+//    console.log(this.$el.find(".canvas"));
   }
 
 });
--- app/static/js/views/history_graph.js
+++ app/static/js/views/history_graph.js
@@ -10,7 +10,36 @@ var HistoryGraph = Backbone.View.extend({
   }
 
 , render: function () {
-    this.$el.html(this.dataDateRange.get("mode") + " history");
+//    this.$el.html(this.dataDateRange.get("mode") + " history");
+
+    $canvas = this.$el.find(".canvas");
+
+
+	var lineChartData = {
+		labels : ["January","February","March","April","May","June","July"],
+		datasets : [
+			{
+				fillColor : "rgba(220,220,220,0.5)",
+				strokeColor : "rgba(220,220,220,1)",
+				pointColor : "rgba(220,220,220,1)",
+				pointStrokeColor : "#fff",
+				data : [65,59,90,81,56,55,40]
+			},
+			{
+				fillColor : "rgba(151,187,205,0.5)",
+				strokeColor : "rgba(151,187,205,1)",
+				pointColor : "rgba(151,187,205,1)",
+				pointStrokeColor : "#fff",
+				data : [28,48,40,19,96,27,100]
+			}
+		]
+		
+	}
+
+	$canvas.attr('height', this.$el.css('height'));
+	$canvas.attr('width', this.$el.css('width'));
+	this.chart = new Chart($canvas[0].getContext("2d")).Line(lineChartData);
+
   }
 
 });
