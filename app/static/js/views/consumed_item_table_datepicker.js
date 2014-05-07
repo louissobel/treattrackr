600b221f6a7487785d8aa17212645fa598091a90
Louis Sobel <sobel@mit.edu>
2014-05-07 14:20:27 -0400

    work on datepicker

--- app/static/js/views/consumed_item_table_datepicker.js
+++ app/static/js/views/consumed_item_table_datepicker.js
@@ -14,12 +14,15 @@ var ConsumedItemTableDatePicker = Backbone.View.extend({
         this.render();
     }
 
-    , goForwardADay: function () {
-        // TODO HANDLE STOPPING AT TODAY.
-        var current = this.model.get("start")
-          , next = new Date(current.getFullYear(), current.getMonth(), current.getDate() + 1)
-          ;
-        this.model.set("start", next);
+    , goForwardADay: function (e) {
+        if ($(e.target).hasClass('enabled')) {
+          var current = this.model.get("start")
+            , next = new Date(current.getFullYear(), current.getMonth(), current.getDate() + 1)
+            ;
+          this.model.set("start", next);
+        } else {
+          // Fuck it, ignore it.
+        }
     }
 
     , goBackADay: function () {
@@ -32,9 +35,19 @@ var ConsumedItemTableDatePicker = Backbone.View.extend({
 
     ,
     render: function () {
-        this.$el.find('.food-add-list-nav-heading').html(
-          moment(this.model.get("start")).format("M/D/YYYY")
-        )
+        var now = new Date()
+          , showing = this.model.get("start")
+          , string
+          ;
+        if (now.getYear() === showing.getYear() && now.getMonth() === showing.getMonth() && now.getDate() === showing.getDate()) {
+          string = "Today";
+          // disable forward nav past today
+          this.$el.find('.right-nav').removeClass('enabled');
+        } else {
+          string = moment(this.model.get("start")).format("M/D/YYYY");
+          this.$el.find('.right-nav').addClass('enabled');
+        }
+        this.$el.find('.food-add-list-nav-heading').html(string);
     }
 
 });
